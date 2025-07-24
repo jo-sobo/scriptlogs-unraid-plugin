b3ac37351a5f5f68a09c3d7ef5be0645507372fd
„Jonas <jonas@sobottka.family>
2025-07-24 13:16:44 +0200

    fixed version

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 # Creates a .txz package for Unraid plugin installation
 
 PLUGIN_NAME="scriptlogs"
-VERSION="2025.01.24a"
+VERSION="2025.07.24a"
 BUILD_DIR="build"
 PACKAGE_NAME="${PLUGIN_NAME}-${VERSION}.txz"
 
